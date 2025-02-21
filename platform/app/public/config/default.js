e72a48f10b2e8e2ad28b7c848d6a1aef07704059
Matthew Heaton <matthew.heaton@bioxydyn.com>
2025-02-21 14:37:31 +0000

    Change PUBLIC_URL

--- platform/app/public/config/default.js
+++ platform/app/public/config/default.js
@@ -3,7 +3,7 @@
 window.config = {
   name: 'config/default.js',
   //routerBasename: '/static/ohif',
-  routerBasename: '/ohif',
+  routerBasename: '/api/ohif',
   whiteLabeling: {
     createLogoComponentFn: function(React) {
       return React.createElement(
